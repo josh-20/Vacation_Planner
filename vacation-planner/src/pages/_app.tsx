ff56a91ef894a1c65497f846b3cadbd3371be851
Joshua <bubbahamby@gmail.com>
2023-04-17 13:31:19 -0600

    update

--- vacation-planner/src/pages/_app.tsx
+++ vacation-planner/src/pages/_app.tsx
@@ -1,6 +1,20 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { useState,useEffect } from 'react';
 
 export default function App({ Component, pageProps }: AppProps) {
+  useEffect (() => { 
+    if(navigator){
+      navigator.geolocation.getCurrentPosition((location) => {
+        console.log(location);
+      }, (err) => {
+        console.log(err);
+      }, {
+        enableHighAccuracy: true,
+      });
+    }else{
+  
+    }
+  },[])
   return <Component {...pageProps} />
 }
