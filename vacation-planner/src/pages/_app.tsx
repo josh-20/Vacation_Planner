b4fa175d60fd9f5104cb102ecb6afd7790feeea9
Joshua <bubbahamby@gmail.com>
2023-04-19 12:30:44 -0600

    base pages

--- vacation-planner/src/pages/_app.tsx
+++ vacation-planner/src/pages/_app.tsx
@@ -3,18 +3,19 @@ import type { AppProps } from 'next/app'
 import { useState,useEffect } from 'react';
 
 export default function App({ Component, pageProps }: AppProps) {
-  useEffect (() => { 
-    if(navigator){
-      navigator.geolocation.getCurrentPosition((location) => {
-        console.log(location);
-      }, (err) => {
-        console.log(err);
-      }, {
-        enableHighAccuracy: true,
-      });
-    }else{
-  
-    }
-  },[])
   return <Component {...pageProps} />
 }
+
+// useEffect (() => { 
+//   if(navigator){
+//     navigator.geolocation.getCurrentPosition((location) => {
+//       console.log(location);
+//     }, (err) => {
+//       console.log(err);
+//     }, {
+//       enableHighAccuracy: true,
+//     });
+//   }else{
+
+//   }
+// },[])
--- vacation-planner/src/pages/api/SignIn.tsx
+++
@@ -1,9 +0,0 @@
-import type { NextApiRequest, NextApiResponse } from 'next'
-
-export const SignIn = () => {
-    return(
-        <div>
-            SignIn Page
-        </div>
-    )
-}
