23f977e2e9ace48e1776eec112d40b5fca268a3e
Joshua <bubbahamby@gmail.com>
2023-04-19 20:52:58 -0600

    working geolocation

--- vacation-planner/src/pages/Planner.tsx
+++ vacation-planner/src/pages/Planner.tsx
@@ -1,6 +1,26 @@
-export const Planner = () => {
+import { watch } from "fs";
+import { useState,useEffect } from "react"
+export default function Planner() {
+    const [longitude,setLongitude] = useState(0);
+    const[latitude,setLatitude] = useState(0);
+
+    useEffect (() => { 
+          const watch = navigator.geolocation.watchPosition((location) => {
+            setLongitude(location.coords.longitude);
+            setLatitude(location.coords.latitude);
+          }, (err) => {
+            console.log(err);
+          }, {
+            enableHighAccuracy: true,
+          });
+          return() => navigator.geolocation.clearWatch(watch);
+      },[])
     return (
         <div>
+            <div>
+                Long: {longitude}
+                Lat: {latitude}
+            </div>
             Planner Page!
         </div>
     )
--- vacation-planner/src/pages/_app.tsx
+++ vacation-planner/src/pages/_app.tsx
@@ -5,17 +5,3 @@ import { useState,useEffect } from 'react';
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
 }
-
-// useEffect (() => { 
-//   if(navigator){
-//     navigator.geolocation.getCurrentPosition((location) => {
-//       console.log(location);
-//     }, (err) => {
-//       console.log(err);
-//     }, {
-//       enableHighAccuracy: true,
-//     });
-//   }else{
-
-//   }
-// },[])
