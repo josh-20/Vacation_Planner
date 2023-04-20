274711e716ab8b5ec3bf9226bbf6a7c0a021110c
ryangoobs24 <ryan24gubler@gmail.com>
2023-04-19 19:27:22 -0600

    made changes

--- vacation-planner/src/pages/_app.tsx
+++ vacation-planner/src/pages/_app.tsx
@@ -1,6 +1,13 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
-
+import Header from './header'
+import 'bootstrap/dist/css/bootstrap.min.css'
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+    <Header />
+    <Component {...pageProps} />
+
+
+  </> )
 }
