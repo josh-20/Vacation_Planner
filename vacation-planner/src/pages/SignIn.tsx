4a050f9fa52edc08d60bec9b56f1e5e42fb6f376
Braeden Grant <bjgrant1996@gmail.com>
2023-04-21 12:52:50 -0600

    Fixed sign-in

--- vacation-planner/src/pages/SignIn.tsx
+++ vacation-planner/src/pages/SignIn.tsx
@@ -1,23 +1,31 @@
 import { useState } from "react"
+import {app} from "../../firebaseConfig"
+import {auth} from "../../firebaseConfig"
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth"
+import {db} from "../../firebaseConfig"
 
 export const SignIn = () => {
 
     const [email,setEmail] = useState("");
     const [Password,setPassword] = useState("");
     async function handleSignIn() {
-        //make fetch to sign in to server.
+        createUserWithEmailAndPassword(auth, email, Password)
+        .catch((error) => {
+            const errorCode = error.code
+            const errorMessage = error.message
+        })
     }
 
     return(
         <div>
             <h1>Sign In</h1>
             <label>
-                Email:
-                <input/>
+            Email:
+                <input value={email} onChange={e => setEmail(e.target.value)} type="email" />
             </label>
             <label>
                 Password:
-                <input/>
+                <input value={Password} onChange={e => setPassword(e.target.value)} type="password" />
             </label>
             <button onClick={handleSignIn}>Sign In</button>
         </div>
