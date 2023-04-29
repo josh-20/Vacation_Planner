c3d89ca0e36a4d1fe37f92d58059c6504a6cee22
Joshua <bubbahamby@gmail.com>
2023-04-28 20:32:27 -0600

    update

--- vacation-planner/src/pages/Home.tsx
+++ vacation-planner/src/pages/Home.tsx
@@ -1,8 +1,17 @@
 import { useState ,useEffect } from "react";
 import { User, getAuth, onAuthStateChanged, signOut } from "firebase/auth";
 import { useRouter } from "next/router";
+import { addDoc,collection } from "firebase/firestore";
+import { db,rtdb } from "../../firebaseConfig";
 import "./SignIn";
 import "./Home";
+
+type Planner = {
+    id: string,
+    creatorId: string,
+    code: string,
+    name: string
+  }
 
 export default function Home() {
     const auth = getAuth();
@@ -10,6 +19,24 @@ export default function Home() {
     const [loggedIn, setLoggedIn] = useState(false);
     const  [user, setUser] = useState<User | null>(null)
     const [loading, setLoading] = useState(true);
+    const [newCode, setCode] = useState('');
+    const [newPlannerName, setNewPlannerName] = useState('');
+    const[planners, setPlanners] = useState<Planner[]>([])
+
+    async function handleCreatePlan() {
+        if(!newCode || !newPlannerName){
+            return;
+          }
+          const planner = {
+            name: newPlannerName,
+            code: newCode,
+            creatorId: auth.currentUser!.uid,
+          }
+           // adding doc for planner. Still need to add A chat room for each planner.
+        const docRef = await addDoc(collection(db, "planner"), planner);
+        (planner as Planner).id = docRef.id;
+        setPlanners([...planners, planner as Planner]);
+    }
 
     useEffect(() => {
         const checkAuth = onAuthStateChanged(auth, (user) =>{
@@ -27,6 +54,9 @@ export default function Home() {
         <div>
             Home Page!
             <button onClick={() => {signOut(auth)}}>signOut</button>
+            <input onChange={e=>setNewPlannerName(e.target.value)}/>
+            <button value="id" onClick={handleCreatePlan}>Create Plan</button>
+
         </div>
     )
 }
--- vacation-planner/src/pages/Planner.tsx
+++ vacation-planner/src/pages/Planner.tsx
@@ -4,7 +4,7 @@ import { Auth, getAuth } from "firebase/auth";
 import { signOut } from "firebase/auth";
 import { query, collection, addDoc, getDocs, where } from "firebase/firestore";
 import {ref, onChildAdded, push, set} from "firebase/database";
-import { db } from "../../firebaseConfig";
+import { db,rtdb } from "../../firebaseConfig";
 
 
 
@@ -14,20 +14,12 @@ type Message = {
   authorEmail: string,
   content: string,
 }
-// planner adapt
-type Planner = {
-  id: string,
-  creatorId: string,
-  code: string,
-  name: string
-}
 
 export default function Planner() {
   const auth = getAuth();
     const [longitude,setLongitude] = useState(0);
     const[latitude,setLatitude] = useState(0);
     const [messages, setMessages] = useState<Message []>([]);
-    const [room, setRoom] = useState<Planner>();
     const [message, setMessage] = useState('');
     const [newPlannerName, setNewRoomName] = useState('');
     const [newCode, setNewCode] = useState('');
@@ -44,24 +36,6 @@ export default function Planner() {
           });
           return() => navigator.geolocation.clearWatch(watch);
       },[])
-
-    useEffect (() => {
-      async function loadChat() {
-        if(!newCode || !newPlannerName){
-          return;
-        }
-        const planner = {
-          name: newPlannerName,
-          code: newCode,
-          creatorId: auth.currentUser!.uid,
-        }
-        // adding doc of planner. still need to 
-        const docRef = await addDoc(collection(db, "planner"), planner);
-        (planner as Planner).id = docRef.id;
-        setRoom(planner as Planner);
-      }
-
-    },[]);
     async function loadChat() {
       const querySnapshot = await getDocs(
         query(
