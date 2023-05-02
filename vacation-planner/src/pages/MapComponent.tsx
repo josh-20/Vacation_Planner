71f0d8955e7150b8449f303e1ffa811a3fb74137
ryangoobs24 <ryan24gubler@gmail.com>
2023-05-02 12:18:01 -0600

    got the markers working for the map component

--- vacation-planner/src/pages/MapComponent.tsx
+++ vacation-planner/src/pages/MapComponent.tsx
@@ -1,15 +1,40 @@
-import { useEffect, useRef } from "react";
+import React, { useState } from "react";
 import { GoogleMap, useLoadScript, Marker } from "@react-google-maps/api";
 
-export default function MapComponent(){
-  const { isLoaded } = useLoadScript({ googleMapsApiKey:  "AIzaSyCkj8pM8eahJEUCXj5Z85FeqiY739s3KJA" as string });
-  if(!isLoaded) return <div>Loading...</div>;
-  return(
-    <Map />
-  )
+export default function MapComponent() {
+  const { isLoaded, loadError } = useLoadScript({
+    googleMapsApiKey: "AIzaSyCkj8pM8eahJEUCXj5Z85FeqiY739s3KJA" as string,
+    libraries: ["places", "drawing", "geometry"],
+  });
+
+  const [center, setCenter] = useState({ lat: 41, lng: -111 });
+  const [markers, setMarkers] = useState<{ lat: number; lng: number }[]>([]);
+
+  const handleMapClick = (event: google.maps.MapMouseEvent) => {
+    const newMarker = {
+      lat: event.latLng!.lat(),
+      lng: event.latLng!.lng(),
+    };
+    setMarkers(current => [...current, newMarker]);
+    setCenter(newMarker);
+  };
+
+  if (loadError) return <div>Error loading maps</div>;
+  if (!isLoaded) return <div>Loading...</div>;
+
+  return (
+    <GoogleMap
+      zoom={4}
+      center={center}
+      mapContainerClassName="map-container"
+      onClick={handleMapClick}
+    >
+      {markers.map((marker, index) => (
+        <Marker key={index} position={{ lat: marker.lat, lng: marker.lng }} />
+      ))}
+    </GoogleMap>
+  );
 }
-function Map() {
-  
-  return <GoogleMap zoom={4} center={{ lat: 41, lng: -111}} mapContainerClassName="map-container">
-  </GoogleMap>
-}
+
+
+
